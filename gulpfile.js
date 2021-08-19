070006e0f65c05e55cb139c9e3a0abb9019f903e
nmanumr <nmanumr@gmail.com>
2021-08-19 22:22:16 +0500

    adds browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ import gulp from 'gulp'
 import rename from 'gulp-rename'
 import buildUnifiedEngine from './tools/engine.js'
 import path from 'path';
+import fs from 'fs';
+import BrowserSync from 'browser-sync';
+
+let browserSync = BrowserSync.create();
 
 export function build(cb, engine=undefined, file="docs/**/*.md") {
     if (!engine) {
@@ -17,20 +21,35 @@ export function build(cb, engine=undefined, file="docs/**/*.md") {
 }
 
 export function watch(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            directory: true
+        }
+    });
+
     const engine = buildUnifiedEngine();
 
     let watcher = gulp.watch('docs/**/*.md');
 
     watcher.on('change', function (path, stats) {
         build(undefined, engine, path);
+        browserSync.reload();
     });
 
     watcher.on('add', function (path, stats) {
         build(undefined, engine, path);
+        browserSync.reload();
     });
 
-    watcher.on('unlink', function (path, stats) {
-        console.log(`File ${path} removed`);
+    watcher.on('unlink', function (file, stats) {
+        const fileName = file.split('/')[file.split('/').length - 1].split('.')[0] + '.html';
+        const dest = path.join('dist', path.dirname(file).split('/').slice(1).join('/'), fileName);
+
+        if (fs.existsSync(dest)) {
+            fs.unlink(dest, () => {});
+        }
+        console.log(`File ${dest} removed`);
     });
 
     build(undefined, engine);
